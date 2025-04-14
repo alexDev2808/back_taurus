d837e642480f9b2ee11c22d352e5601baa74c8f5
J Alexis <colores.2808.8000@gmail.com>
2025-04-14 16:33:33 -0600

    Autenticacion

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'password' => 'required|string',
+            'email' => 'required|email',
+        ], [
+            'email.required' => 'El campo correo es obligatorio.',
+            'email.email' => 'El correo no tiene el formato correcto.',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], Response::HTTP_BAD_REQUEST);
+        }
+
+        $credentials = request(['email', 'password']);
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Datos de acceso incorrectos. Por favor, verifica tus credenciales.'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function unauthorized()
+    {
+        return redirect(route('login'));
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100|min:2',
+            'password' => 'required|string|max:255|min:8',
+            'email' => 'required|email|unique:users,email',
+        ], [
+            'name.required' => 'El campo nombre es obligatorio.',
+            'name.min' => 'El nombre debe de tener al menos min caracteres.',
+            'name.max' => 'El nombre no puede tener más de max caracteres.',
+            'email.required' => 'El campo correo es obligatorio.',
+            'email.unique' => 'El correo ya está registrado.',
+            'email.email' => 'El correo no tiene el formato correcto.',
+            'password.required' => 'El campo contraseña es obligatorio.',
+            'password.min' => 'El campo contraseña es de mínimo 8 caracteres.',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], Response::HTTP_BAD_REQUEST);
+        }
+
+        $exists = User::where('email', htmlspecialchars($request->input('email')))->first();
+        if (!$exists) {
+            $new = User::create([
+                'name' => htmlspecialchars($request->input('name')),
+                'email' => htmlspecialchars($request->input('email')),
+                'password' => Hash::make($request->input('password')),
+                'rol' => 'CLIENTE',
+                'empresa_id' => null,
+            ]);
+
+            if (!$new) {
+                return response()->json(['error' => 'No se logró crear'], Response::HTTP_INTERNAL_SERVER_ERROR);
+            }
+
+            return response()->json($new, Response::HTTP_CREATED);
+        } else {
+            return response()->json(['error' => 'Ya existe un usuario con ese email'], Response::HTTP_BAD_REQUEST);
+        }
+    }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        try {
+            $token = JWTAuth::getToken(); // Obtiene el token actual
+            if (!$token) {
+                return response()->json(['error' => 'Token no encontrado'], Response::HTTP_BAD_REQUEST);
+            }
+
+            // Invalida el token actual
+            JWTAuth::invalidate($token);
+
+            return response()->json(['message' => 'Sesión cerrada correctamente'], Response::HTTP_OK);
+        } catch (TokenInvalidException $e) {
+            return response()->json(['error' => 'Token inválido'], Response::HTTP_UNAUTHORIZED);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'No se pudo cerrar la sesión'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        try {
+            $token = JWTAuth::getToken(); // Obtiene el token actual
+            if (!$token) {
+                return response()->json(['error' => 'Token no encontrado'], Response::HTTP_BAD_REQUEST);
+            }
+
+            $nuevo_token = auth()->refresh();
+            // Invalida el token actual
+            JWTAuth::invalidate($token);
+            return $this->respondWithToken($nuevo_token);
+        } catch (TokenInvalidException $e) {
+            return response()->json(['error' => 'Token inválido'], Response::HTTP_UNAUTHORIZED);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'No se pudo cerrar la sesión'], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ], Response::HTTP_OK);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('register', [AuthController::class, 'register']);
+        Route::post('login', [AuthController::class, 'login']);
+
+        Route::middleware('auth:api')->group(function () {
+            Route::get('me', [AuthController::class, 'me']);
+            Route::post('refresh', [AuthController::class, 'refresh']);
+            Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+        });
+    });
+
+    Route::middleware('auth:api')->group(function () {
+        Route::resource('users', UserController::class);
+    });
+});
+
+Route::get('/', [AuthController::class, 'unauthorized'])->name('login');
